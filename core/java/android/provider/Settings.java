459a239a5fbb013fce3120ee7f1a7370a34d7a81
Stevespear426 <stephen.k.spear@gmail.com>
2016-05-20 12:26:17 +0200

    AOKP custom system animations [1/2]
    	[2/2] in packages/apps/Settings
    
    - Work by AOKP team
    - Ported by @cristianomatos @crdroidandroid
    
    Signed-off-by: OwnDroid <askowndroid@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values-nl/cm_strings.xml
    	core/res/res/values/cm_strings.xml
    	core/res/res/values/cm_symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3821,6 +3821,24 @@ public final class Settings {
         @Deprecated
         public static final String BLUETOOTH_ON = Global.BLUETOOTH_ON;
 
+        /**  
+         * AOKP Custom System Animations
+         * @hide
+         */  
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {  
+                "activity_open",
+                "activity_close",
+                "task_open",  
+                "task_close",  
+                "task_to_front",
+                "task_to_back",  
+                "wallpaper_open",  
+                "wallpaper_close",  
+                "wallpaper_intra_open",  
+                "wallpaper_intra_close",  
+        };  
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+
         /**
          * @deprecated Use {@link android.provider.Settings.Global#DATA_ROAMING} instead
          */
